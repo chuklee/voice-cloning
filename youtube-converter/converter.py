33d52369508ccb4a5ff994776ef5620e2f7525cc
Pierre-Louis <pierre-louis.favreau@epita.fr>
2024-09-15 15:58:28 +0200

    feat: dockerized converter.py

--- youtube-converter/converter.py
+++ youtube-converter/converter.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import yt_dlp
 from moviepy.editor import AudioFileClip
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 @app.route('/')
 def home():
     return "Flask is running!"
-# Fonction pour télécharger l'audio avec yt-dlp
+
 def download_audio_from_youtube(url):
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -23,18 +26,16 @@ def download_audio_from_youtube(url):
         ydl.download([url])
     return 'audio.mp3'
 
-# Fonction pour convertir le fichier téléchargé en .wav
 def convert_to_wav(input_file):
     output_file = os.path.splitext(input_file)[0] + '.wav'
     audio_clip = AudioFileClip(input_file)
-    audio_clip.write_audiofile(output_file, codec='pcm_s16le')  # Codec pour .wav
+    audio_clip.write_audiofile(output_file, codec='pcm_s16le')
     audio_clip.close()
     return output_file
 
-# Route pour gérer la requête POST
 @app.route('/convert', methods=['POST'])
 def convert_youtube_to_wav():
-    # Extraction de l'URL YouTube depuis le corps de la requête
+    print("Received request with parameters : ", request.get_json())
     data = request.get_json()
     youtube_url = data.get('youtube_url')
 
@@ -42,17 +43,14 @@ def convert_youtube_to_wav():
         return jsonify({'error': 'No YouTube URL provided'}), 400
 
     try:
-        # Télécharger l'audio depuis YouTube
         audio_file = download_audio_from_youtube(youtube_url)
-        
-        # Convertir l'audio téléchargé en .wav
         wav_file = convert_to_wav(audio_file)
 
-        # Réponse avec le fichier .wav
-        return jsonify({'message': f'Audio .wav enregistré sous : {wav_file}'})
+        return send_file(wav_file, as_attachment=True, download_name='audio.wav', mimetype='audio/wav')
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print("Starting Flask app") 
+    app.run(debug=True, host='0.0.0.0', port=3333)
